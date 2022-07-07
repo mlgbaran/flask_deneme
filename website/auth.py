4f4229f041c7eddf2fce6ae86397e1f8b2ebb6df
Baran Çiçek <mlgbaran@gmail.com>
2022-07-07 03:05:19 +0300

    deneme

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 
 auth = Blueprint('auth', __name__)
@@ -18,6 +20,8 @@ def login():
         if user:
             if check_password_hash(user.password,password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -48,7 +52,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1,method="sha256"))
             db.session.add(new_user)
             db.session.commit()
-            
+
             flash('Account Created!', category='success')
             return redirect(url_for('views.home'))
     return render_template("sign_up.html")
