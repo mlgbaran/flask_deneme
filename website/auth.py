4d149c80979c51d65af5f9183701a9671ab411fd
Baran Çiçek <mlgbaran@gmail.com>
2022-07-08 04:08:44 +0300

    small changes

--- website/auth.py
+++ website/auth.py
@@ -5,6 +5,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+import MySQLdb
 
 from flask_sqlalchemy import SQLAlchemy
 
